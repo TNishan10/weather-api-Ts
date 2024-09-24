87532ac853ae0dae8e8a389444549a866ec04d44
Nishan Thapa <tnishan110@gmail.com>
2024-09-24 20:39:51 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Select from "react-select";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Side Bar*/}
+      <div>
+        {/*Form*/ }
+        <Select />
+
+        <button className="bg-green-400 w-full py-3 text-white text-sm font-bold hover:scale-105 transition-all duration-200 ease-in-out">Get Weather</button>
+       </div>
+      {/*Body */}
+      <div>
+
+        </div>
     </div>
   );
 }
